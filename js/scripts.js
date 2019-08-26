f616498699bf6297e2147a1482b3b7418835e030
Lauren Musante and Jen Batara <example@example.com>
2019-08-26 09:24:51 -0700

    Initialize project. Add beginning JS objects.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function AddressBook(contact){
+  this.currentID=0;
+  this.contacts=[];
+}
+ function Contacts(first, last, phone){
+   this.firstName = first;
+   this.lastName = last;
+   this.phoneNumber = phone;
+ }
