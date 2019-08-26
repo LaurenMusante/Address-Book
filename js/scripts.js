777939fdfedc95524eb10233c74fa8af7b4c4040
Lauren Musante and Jen Batara <example@example.com>
2019-08-26 10:49:24 -0700

    Add UI logic and HTML

--- js/scripts.js
+++ js/scripts.js
@@ -18,12 +18,58 @@ AddressBook.prototype.addContact = function(contact) {
   return contact;
 }
 
+AddressBook.prototype.findContact = function(id) {
+  for (var i = 0; i<this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (var i=0; i<this.contacts.length; i++) {
+    if (this.contacts[i]){
+      if(this.contacts[i].id = id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
 AddressBook.prototype.assignId = function(){
   this.currentId += 1;
   return this.currentId;
 }
 
+AddressBook.prototype.findContact
+
 var nina = new Contact ("Nina", "Murphy", "8318880000");
 var rj = new Contact ("RJ", "Leimpeter", "5109873019");
 var someChick = new Contact ("Vanessa", "Shoemaker", "5038675309");
 var someDude = new Contact ("Chad", "Michaels", "5082037045");
+
+// UI Logic
+var addressBook = new AddressBook();
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event){
+    event.preventDefault();
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedPhoneNumber = $("input#new-phone-number").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    console.log(addressBook.contacts);
+
+
+    $("span.first-name").text(inputtedFirstName);
+    $("span.last-name").text(inputtedLastName);
+    $("span.phone-number").text(inputtedPhoneNumber);
+    $("#show-contact").show();
+  });
+});
