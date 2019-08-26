bcc4026c7d735c008dca2d12f4b0e00c074d2fe3
Lauren Musante and Jen Batara <example@example.com>
2019-08-26 09:37:15 -0700

    Add fullName, addContact, and assignID methods in JS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function AddressBook(contact){
-  this.currentID=0;
+  this.currentId=0;
   this.contacts=[];
 }
  function Contacts(first, last, phone){
@@ -7,3 +7,17 @@ function AddressBook(contact){
    this.lastName = last;
    this.phoneNumber = phone;
  }
+
+Contacts.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts.push(contact);
+  return contact;
+}
+
+AddressBook.prototype.assignId = function(){
+  this.currentId += 1;
+  return this.currentId;
+}
